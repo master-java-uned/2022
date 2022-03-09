2160eec30937184a8579e1e86c86f369fdfb45e7
redeskako <carlosl.sanchez@gmail.com>
2022-03-09 14:15:43 +0100

    Añadiendo la enum de Estados y sus test respectivos.

---
+++ suministros/src/main/java/es/uned/master/java/enumerados/Estado.java
@@ -0,0 +1,5 @@
+package es.uned.master.java.enumerados;
+
+public enum Estado {
+    DISPONIBLE, ALQUILADO, ENREPARACION;
+}
--- suministros/src/test/java/es/uned/master/java/test/EnumeradosTest.java
+++ suministros/src/test/java/es/uned/master/java/test/EnumeradosTest.java
@@ -5,10 +5,16 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 
-public class CategoriaTest {
+public class EnumeradosTest {
     @Test
     public void esCategoria(){
         Categoria c = Categoria.LIMPIEZA;
         assertTrue(c instanceof Categoria);
+    }
+
+    @Test
+    public void esEstado() {
+        Estado e = Estado.ALQUILADO;
+        assertTrue(e instanceof Estado);
     }
 }
